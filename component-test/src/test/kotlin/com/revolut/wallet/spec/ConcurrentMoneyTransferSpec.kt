80cd5b4d9aeb0ed87ff169c99775541ee86e70eb
Rishat Babyshev <b.rishat@gmail.com>
2019-08-07 12:36:52 +0600

    Add concurrent test cases

--- component-test/src/test/kotlin/com/revolut/wallet/spec/ConcurrentMoneyTransferSpec.kt
+++ component-test/src/test/kotlin/com/revolut/wallet/spec/ConcurrentMoneyTransferSpec.kt
@@ -15,7 +15,7 @@ class ConcurrentMoneyTransferSpec : DescribeSpec({
     describe("Concurrent money transfer") {
         val account1 = createAccount(100.0)
         val account2 = createAccount(100.0)
-        val account3 = createAccount()
+        val account3 = createAccount(100.0)
         val overallBalance = listOf(account1, account2, account3).sumByDouble { it.balance }
 
         context("Check account 1") {
@@ -41,11 +41,11 @@ class ConcurrentMoneyTransferSpec : DescribeSpec({
             val account = mapper.readValue(result, Account::class.java)
             it("Account 3 created") {
                 account.id shouldBe account3.id
-                account.balance shouldBe 0.0
+                account.balance shouldBe 100.0
             }
         }
 
-        context("Transfer 60 from account1 to account2 and from account1 to account3") {
+        context("Transfer a1 -> a3, a2 -> a3") {
 
             it("should create transaction").config(invocations = 50, parallelism = 5) {
                 val transaction1Id = transfer(account1, account3, 1)
@@ -53,6 +53,70 @@ class ConcurrentMoneyTransferSpec : DescribeSpec({
                 logger.info { transaction1Id }
                 logger.info { transaction2Id }
                 listOfNotNull(transaction1Id, transaction2Id).size shouldBeLessThanOrEqual 2
+            }
+
+            delay(5000)
+
+            context("Check account 3 balance change") {
+                val result1 = client.get<String>("account/${account1.id}")
+                val account1Balance = mapper.readValue(result1, Account::class.java).balance
+
+                val result2 = client.get<String>("account/${account2.id}")
+                val account2Balance = mapper.readValue(result2, Account::class.java).balance
+
+                val result3 = client.get<String>("account/${account3.id}")
+                val account3Balance = mapper.readValue(result3, Account::class.java).balance
+
+                it("Overall amount of money should be $overallBalance") {
+                    (account1Balance + account2Balance + account3Balance) shouldBe overallBalance
+                }
+                logger.info { "${account1.id}: $account1Balance" }
+                logger.info { "${account2.id}: $account2Balance" }
+                logger.info { "${account3.id}: $account3Balance" }
+            }
+        }
+
+        context("Transfer a1 -> a2, a1 -> a3") {
+
+            it("should create transaction").config(invocations = 50, parallelism = 5) {
+                val transaction1Id = transfer(account1, account2, 1)
+                val transaction2Id = transfer(account1, account3, 1)
+                logger.info { transaction1Id }
+                logger.info { transaction2Id }
+                listOfNotNull(transaction1Id, transaction2Id).size shouldBeLessThanOrEqual 2
+            }
+
+            delay(5000)
+
+            context("Check account 3 balance change") {
+                val result1 = client.get<String>("account/${account1.id}")
+                val account1Balance = mapper.readValue(result1, Account::class.java).balance
+
+                val result2 = client.get<String>("account/${account2.id}")
+                val account2Balance = mapper.readValue(result2, Account::class.java).balance
+
+                val result3 = client.get<String>("account/${account3.id}")
+                val account3Balance = mapper.readValue(result3, Account::class.java).balance
+
+                it("Overall amount of money should be $overallBalance") {
+                    (account1Balance + account2Balance + account3Balance) shouldBe overallBalance
+                }
+                logger.info { "${account1.id}: $account1Balance" }
+                logger.info { "${account2.id}: $account2Balance" }
+                logger.info { "${account3.id}: $account3Balance" }
+            }
+        }
+
+        context("Transfer a1 -> a2, a2 -> a3, a3 -> a1") {
+
+            it("should create transaction").config(invocations = 50, parallelism = 5) {
+                val transaction1Id = transfer(account1, account2, 1)
+                val transaction2Id = transfer(account2, account3, 1)
+                val transaction3Id = transfer(account3, account1, 3)
+                logger.info { transaction1Id }
+                logger.info { transaction2Id }
+                logger.info { transaction3Id }
+                listOfNotNull(transaction1Id, transaction2Id, transaction3Id).size shouldBeLessThanOrEqual 3
             }
 
             delay(5000)
